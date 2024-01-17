f7c08388c310492b0f0c7215fa32396280b76191
Megan <megan@MacBook-Air-von-Megan.local>
2024-01-17 12:52:03 +0100

    modified Controller file

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
 class QuestionController
 {
+    #[Route('/')]
+    public function homepage() {
+        return new Response('welcome test');
+    }
 
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null) {
+
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug)) -> title(true);
+        }
+        else {
+            $title = 'All Genres';
+        }
+
+        return new Response($title);
+    }
 }
+
+// u and title(true) turns every first letter within a string into Uppercase
+
+//#[Route("/articles/{category}/{page}/{highlight}")]
+//public function article($page, $category): Response
+//{
+//}
